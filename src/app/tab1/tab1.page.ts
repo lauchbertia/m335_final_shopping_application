ea7aaa4559fff91ef1b46d8ceef6b80c620ee3b7
lauchbertia <sabrina.boccia@bbzbl-it.ch>
2023-11-24 11:02:06 +0100

    tabs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ConfiguratorComponent } from '../configurator/configurator.component';
+import { GeolocatorComponent } from '../geolocator/geolocator.component';
+import { BatteryStatusComponent } from '../battery-status/battery-status.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ConfiguratorComponent],
+  imports: [IonicModule, ConfiguratorComponent, GeolocatorComponent,BatteryStatusComponent],
 })
 export class Tab1Page {
   constructor() {}
